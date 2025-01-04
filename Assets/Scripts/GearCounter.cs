53700613a58f63a00c00528d8b4ac2339f058137
amnaashraf0 <ashrafamna000@gmail.com>
2025-01-03 18:40:55 -0600

    Identify which gears are on which pole

---
+++ Assets/Scripts/GearCounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GearCounter : MonoBehaviour
+{
+    public GearManager manager;
+    public XRSocketInteractor interactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addGear() {
+        if (manager != null)
+        {
+            IXRSelectInteractable interactable = interactor.GetOldestInteractableSelected();
+            GameObject gear = interactable.transform.gameObject;
+            manager.addSharedGears(gear, gameObject);
+        }
+        else {
+            Debug.Log("No gear manager");
+        }
+    }
+}
---
+++ Assets/Scripts/GearManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GearManager : MonoBehaviour
+{
+    private int gearCounter = 0;
+    private int[] gears = new int[2];
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addSharedGears(GameObject gear, GameObject triggeringObject)
+    {
+        int teethNumber  = int.Parse(gear.transform.name.Substring(0, 2));
+        if (triggeringObject.transform.name == "Pole 1")
+        {
+            gears[0] = teethNumber;
+        }
+        else if (triggeringObject.transform.name == "Pole 4")
+        {
+            gears[1] = teethNumber;
+        }
+        gearCounter++;
+        if (gearCounter == 4) {
+            Debug.Log("Pole 1: " + gears[0]);
+            Debug.Log("Pole 4: " + gears[1]);
+        }
+    }
+}
