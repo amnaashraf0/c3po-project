0484aa9f2d88e89347f1832b86ea3ccf02738547
shru124 <konuru.shruti@gmail.com>
2025-01-26 11:38:35 -0600

    Trying the ima script

--- Assets/Scripts/IMAScript.cs
+++ Assets/Scripts/IMAScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,8 @@ public class IMAScript : MonoBehaviour
     // other managers ex: incline, screw
     private double totalIMA = 0;
     private double gearTrainIMA = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/IMAUpdater.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;  // Required for Text UI
+
+public class IMAUpdater : MonoBehaviour
+{
+    public IMAScript imaScript;  // Reference to IMAScript to get the IMA value
+    public Text uiText;          // Reference to the Text UI component
+    public GearManager gearManager;  // Reference to GearManager to get the IMA value
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gearManager != null && uiText != null)
+        {
+            // Directly call the getIMA() method from GearManager
+            double ima = gearManager.getIMA();
+            uiText.text = "IMA: " + ima.ToString("F2");  // Update the text with the IMA value
+        }
+    }
+}
