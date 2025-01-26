dad544ecd1e8fdc9ebb6b4a627b995afea52c7b2
amnaashraf0 <ashrafamna000@gmail.com>
2025-01-26 12:28:10 -0600

    Fix small text bugs

--- Assets/Scripts/IMAUpdater.cs
+++ Assets/Scripts/IMAUpdater.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;  // Required for Text UI
 
 public class IMAUpdater : MonoBehaviour
 {
     public IMAScript imaScript;  // Reference to IMAScript to get the IMA value
-    public Text uiText;          // Reference to the Text UI component
+    [SerializeField] TextMeshProUGUI uiText;      // Reference to the Text UI component
     public GearManager gearManager;  // Reference to GearManager to get the IMA value
 
     void Start()
@@ -21,7 +22,7 @@ public class IMAUpdater : MonoBehaviour
         {
             // Directly call the getIMA() method from GearManager
             double ima = gearManager.getIMA();
-            uiText.text = "" + ima.ToString("F2");  // Update the text with the IMA value
+            uiText.text = "Total IMA:\n" + ima.ToString("F2");  // Update the text with the IMA value
         }
     }
 }
